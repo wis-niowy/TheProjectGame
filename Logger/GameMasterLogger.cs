286803cb449a9d218ea6ef876b1fd82e4fa52cf8
AleksWasiluk <alek.wasiluk@gmail.com>
2018-03-24 11:34:11 +0100

    Added Tests and new methods for Loggers

---
+++ Logger/GameMasterLogger.cs
@@ -0,0 +1,289 @@
+﻿using GameArea;
+using Loggers;
+using Messages;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loggers
+{
+    public class GameMasterLogger : Logger
+    {
+        private Logger mainLogger;
+        private new StreamWriter sw;
+        private string nameGameMasterLogger;
+       // private string nameFileGameMaster;
+
+        public GameMasterLogger()
+        {
+            objectName = "GameMaster";
+            mainLogger = Logger.Instance;
+            nameGameMasterLogger = "GMlogs.txt";
+           
+        }
+
+        public GameMasterLogger(string nameFileGameMaster)
+        {
+            this.nameGameMasterLogger = nameFileGameMaster;
+            objectName = "GameMaster";
+            mainLogger = Logger.Instance;
+        }
+
+        public void LogBoardPieces(GameMaster gameMaster)
+        {
+            sw = new StreamWriter(nameGameMasterLogger, true);
+            Board board = gameMaster.GetBoard;
+            DrawBoardPieces(board);
+            sw.Close();
+        }
+        public void LogBoardAgents(GameMaster gameMaster)
+        {
+            sw = new StreamWriter(nameGameMasterLogger, true);
+            Board board = gameMaster.GetBoard;
+            DrawBoardAgents(board);
+            sw.Close();
+        }
+
+        //This method draws agents and ignored fields on a board
+        protected void DrawBoardAgents(Board board)//with GoalField
+        {
+            string line = Const.line;
+            uint TaskAreaHeight = board.TaskAreaHeight;
+            uint BoardWidth = board.BoardWidth;
+            uint GoalAreaHeight = board.GoalAreaHeight;
+
+            //indexing height from 0
+            sw.WriteLine("Board :");
+            sw.WriteLine("");
+            //-------------------------------------------
+            uint startHeight = TaskAreaHeight + 2 * GoalAreaHeight - 1;
+            uint endHeight = TaskAreaHeight + GoalAreaHeight - 1;
+            //-------------- Red Goal Area ----------------
+            sw.WriteLine($"{line} Red Goal Area {line}");
+
+            for (uint height = startHeight; height > endHeight; height--)
+            {
+                string newLine = $"{height} ";//index of height
+
+                for (uint width = 0; width < GoalAreaHeight; width++)
+                {
+                    //------------drawing objects--------------------------------
+
+
+
+                    var field = board.GetField(width, height);
+
+                    if (field.HasAgent())
+                    {
+                        var player = field.Player;
+                        newLine += DrawAgent(player);
+                    }
+                    else
+                    {
+                        
+                        newLine += DrawIgnoredField();
+                    }
+
+
+                    newLine += " ";
+                }
+
+                sw.WriteLine(newLine);
+            }
+            sw.WriteLine($"{line} Task Area {line}");
+            //-------------------------------------------
+            startHeight = TaskAreaHeight + GoalAreaHeight - 1;
+            endHeight = GoalAreaHeight - 1;
+            //-------------- Task Area ----------------
+            for (uint height = startHeight; height > endHeight; height--)
+            {
+                string newLine = $"{height} ";
+                for (uint width = 0; width < BoardWidth; width++)
+                {
+                    //------------drawing objects--------------------------------
+
+                    var field = board.GetField(width, height);
+
+                    if (field.HasAgent())
+                    {
+                        var player = field.Player;
+                        newLine += DrawAgent(player);
+                    }
+                    else
+                    {
+                        newLine += DrawIgnoredField();
+                    }
+
+                    newLine += " ";
+                }
+                sw.WriteLine(newLine);
+            }
+            sw.WriteLine($"{line} Blue Goal Area {line}");
+
+            //-------------------------------------------
+            startHeight = GoalAreaHeight - 1;
+            endHeight = 0;
+            //-------------- Blue Goal Area ----------------
+            for (uint height = startHeight; height >= endHeight; height--)//>= because endHeight == 0
+            {
+                string newLine = $"{height} ";//index of height
+
+                for (uint width = 0; width < GoalAreaHeight; width++)
+                {
+                    //------------drawing objects--------------------------------
+
+                    var field = board.GetField(width, height);
+
+                    if (field.HasAgent())
+                    {
+                        var player = field.Player;
+                        newLine += DrawAgent(player);
+                    }
+                    else
+                    {
+                        newLine += DrawIgnoredField();
+                    }
+
+
+                    newLine += " ";
+                }
+                sw.WriteLine(newLine);
+                if (height == 0)
+                    break;
+            }
+            sw.WriteLine($"{line}{line}");
+        }
+
+
+
+
+
+        private string DrawIgnoredField()
+        {
+            return "[ig]";
+        }
+
+
+
+
+
+        protected void DrawBoardPieces( Board board)//with GoalField
+        {
+            string line = Const.line;
+            uint TaskAreaHeight = board.TaskAreaHeight;
+            uint BoardWidth = board.BoardWidth;
+            uint GoalAreaHeight = board.GoalAreaHeight;
+
+            //indexing height from 0
+            sw.WriteLine("Board :");
+            sw.WriteLine("");
+            //-------------------------------------------
+            uint startHeight = TaskAreaHeight + 2 * GoalAreaHeight - 1;
+            uint endHeight = TaskAreaHeight + GoalAreaHeight - 1;
+            //-------------- Red Goal Area ----------------
+            sw.WriteLine($"{line} Red Goal Area {line}");
+
+            for (uint height = startHeight; height > endHeight; height--)
+            {
+                string newLine = $"{height} ";//index of height
+
+                for (uint width = 0; width < GoalAreaHeight; width++)
+                {
+                    //------------drawing objects--------------------------------
+
+
+                    var field = board.GetField(width, height);
+
+                    //if on field there is any agent he will be drawn instead of field
+                    
+
+                    var goalField = field as GameArea.GoalField;
+                    newLine += DrawGoalField(goalField);
+
+
+
+                    newLine += " ";
+                }
+
+                sw.WriteLine(newLine);
+            }
+            sw.WriteLine($"{line} Task Area {line}");
+            //-------------------------------------------
+            startHeight = TaskAreaHeight + GoalAreaHeight - 1;
+            endHeight = GoalAreaHeight - 1;
+            //-------------- Task Area ----------------
+            for (uint height = startHeight; height > endHeight; height--)
+            {
+                string newLine = $"{height} ";
+                for (uint width = 0; width < BoardWidth; width++)
+                {
+                    //------------drawing objects--------------------------------
+                    //location logger's player
+                    var field = board.GetField(width, height);
+
+                    //if on field there is any agent he will be drawn instead of field
+                   
+                    var taskField = field as GameArea.TaskField;
+                    newLine += DrawMasterTaskField(taskField);
+
+
+                    newLine += " ";
+                }
+                sw.WriteLine(newLine);
+            }
+            sw.WriteLine($"{line} Blue Goal Area {line}");
+
+            //-------------------------------------------
+            startHeight = GoalAreaHeight - 1;
+            endHeight = 0;
+            //-------------- Blue Goal Area ----------------
+            for (uint height = startHeight; height >= endHeight; height--)//>= because endHeight == 0
+            {
+                string newLine = $"{height} ";//index of height
+
+                for (uint width = 0; width < GoalAreaHeight; width++)
+                {
+                    //------------drawing objects--------------------------------
+                    //location logger's player
+                    var field = board.GetField(width, height);
+
+                    //if on field there is any agent he will be drawn instead of field
+                   
+                    var goalField = field as GameArea.GoalField;
+                    newLine += DrawGoalField(goalField);
+
+
+                    newLine += " ";
+                }
+                sw.WriteLine(newLine);
+                if (height == 0)
+                    break;
+            }
+            sw.WriteLine($"{line}{line}");
+
+
+        }
+
+
+        public void Log(GameMaster gameMaster, string decision, string description)
+        {
+            mainLogger.Log(decision, description);
+
+            StreamWriter sw = new StreamWriter(nameGameMasterLogger, true);
+            var dt = DateTime.Now;
+            string time = String.Format(Const.timeFormat, dt);
+
+            sw.WriteLine($"{time} {objectName} {decision} {description}");
+            sw.Close();
+        }
+
+
+
+
+
+    }
+
+}
--- Logger/Logger.cs
+++ Logger/Logger.cs
@@ -1,16 +1,33 @@
-﻿using System;
+﻿using GameArea;
+using Messages;
+using Player;
+using System;
 using System.IO;
 using System.Text;
 using System.Xml;
-namespace Logger
+
+namespace Loggers
 {
+
+
+
+
     public class Logger
     {
+        protected class Const
+        {
+            public const string timeFormat = "{0:yyyy-mm-ddTHH:mm:ss.fff}";
+            public const string line = "------------------";
+
+        }
+
         // note: StreamWriter is not thread safe
         protected static StreamWriter sw; // main log
         private static Logger instance;
         protected Logger() { }
         protected static int counter = 0;
+        protected string objectName = "";
+        protected static string nameLog = null;
 
         public static Logger Instance
         {
@@ -29,55 +46,128 @@ namespace Logger
             if (instance == null)
             {
                 instance = new Logger();
-                sw = new StreamWriter(filename, true);
+                nameLog = filename;
+                //sw = new StreamWriter(filename, true);
             }
         }
 
-        public void Log(string xml)
+        public void Log(string action,string description)
         {
+            sw = new StreamWriter(nameLog, true);
             var dt = DateTime.Now;
-            string str = String.Format("{0:yyyy-mm-ddTHH:mm:ss.fff}", dt);
-            sw.WriteLine(str + " " + xml);
+            string time = String.Format(Const.timeFormat, dt);
+            sw.WriteLine($"{time} {objectName} {action} {description}");
+            sw.Close();
         }
+
+        //method used for drawing goalfield with description in the board
+        protected string DrawGoalField(GameArea.GoalField field)
+        {
+            string stringField = "";
+            var goal = field.GoalType;
+
+            stringField += "[";
+            switch (goal)
+            {
+                case GoalFieldType.goal:
+                    stringField += "gf";
+                    break;
+                case GoalFieldType.nongoal:
+                    stringField += "ng";
+                    break;
+                case GoalFieldType.unknown:
+                    stringField += "uk";
+                    break;
+                default:
+
+                    break;
+            }
+            stringField += "]";
+
+            return stringField;
+        }
+
+        //method used for drawing agent with description in the board
+        protected string DrawAgent(Messages.Agent player)
+        {
+            string stringAgent = "";
+            stringAgent += "[";
+            if (player.team == TeamColour.red)
+                stringAgent += $"r";
+            else //player.team == TeamColour.blue
+                stringAgent += $"b";
+
+            if (player.type == PlayerType.leader)
+                stringAgent += $"l";
+            else //player.team == PlayerType.agent
+                stringAgent += $"a";
+
+            stringAgent += "]";
+
+            return stringAgent;
+        }
+        protected string DrawMasterTaskField(GameArea.TaskField field)
+        {
+            string stringField = "";
+            stringField += "[";
+
+            if (field.GetPiece != null)
+            {
+                Piece piece = field.GetPiece;
+
+                switch(piece.type)
+                {
+                    case PieceType.normal:
+                        stringField += "vp";//there is piece
+                        break;
+                    case PieceType.sham:
+                        stringField += "sp";//there is sham
+                        break;
+                    case PieceType.unknown:
+                        stringField += "uk";//there is uknown
+                        break;
+                }
+
+                
+            }
+            else
+            {
+                stringField += "ef";//there is no any piece
+            }
+
+            stringField += "]";
+            return stringField;
+        }
+
+
+
+        ////method used for drawing taskfield with description in the board for Agent
+        protected string DrawTaskField(GameArea.TaskField field)
+        {
+            string stringField = "";
+            stringField += "[";
+
+            if (field.GetPiece != null)
+            {
+                stringField += "pf";//there is piece
+            }
+            else
+            {
+                stringField += "ef";//there is no any piece
+            }
+
+            stringField += "]";
+            return stringField;
+        }
+
     }
 
-    public class GameMasterLogger : Logger
-    {
-        private Logger mainLogger;
-        private new StreamWriter sw;
 
-        public GameMasterLogger()
-        {
-            mainLogger = Logger.Instance;
-            StreamWriter sw = new StreamWriter("GMlogs.txt", true);
-        }
 
-        public void Log(string xml)
-        {
-            // Deserialize xml
-            mainLogger.Log(xml);
-        }
-    }
+   
+    
 
-    public class PlayerLogger : Logger
-    {
-        private Logger mainLogger;
-        private new StreamWriter sw;
-        private int myId;
 
-        public PlayerLogger()
-        {
-            myId = counter++;
-            mainLogger = Logger.Instance;
-            StreamWriter sw = new StreamWriter("log" + myId + ".txt", true);
-        }
 
-        public new void Log(string xml)
-        {
-            mainLogger.Log(xml);
-            var dt = DateTime.Now;
-            string str = String.Format("{0:yyyy-mm-ddTHH:mm:ss.fff}", dt);
-            sw.WriteLine(str + " " + xml);
-        }
-    }
+    
 }
