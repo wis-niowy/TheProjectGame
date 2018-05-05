37af66bd93f93342a253ed54aa94a24cb32fb3c7
TwistedAnckle <jachacym@student.mini.pw.edu.pl>
2018-05-05 07:16:17 +0200

    server args validation

--- CommunicationServer/CS.cs
+++ CommunicationServer/CS.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace CommunicationServer
@@ -50,46 +51,83 @@ namespace CommunicationServer
         }
         static void Main(string[] args)
         {
-            var valid = ValidateArgs(args);
-            if (!valid)
-                ConsoleWriter.Warning("Invalid args, laoding default!");
             IPAddress IP;
-            int socket;
-            if (valid)
+            int port;
+            int year;
+            string lang;
+            var valid = ValidateArgs(args, out port, out IP, out year, out lang);
+            if (!valid)
             {
-                IP = IPAddress.Parse(args[0]);
-                socket = Int32.Parse(args[1]);
-            }
-            else
-            {
+                ConsoleWriter.Warning("Invalid args, loading default!");
+                lang = "en";
+                year = 18;
                 IP = IPAddress.Parse("127.0.0.1");
-                socket = Int32.Parse("5678");
+                port = 5678;
             }
-            TcpHelper.StartServer(IP, socket);
+            TcpHelper.StartServer(IP, port);
             TcpHelper.Listen(); // Start listening. 
         }
 
-        private static bool ValidateArgs(string[] args)
+        private static bool ValidateArgs(string[] args, out int portNumber, out IPAddress serverAddress, out int year, out string lang)
         {
-            var valid = true;
-            if (args.Length < 2)
+            bool validAddress = false;
+            bool validPort = false;
+            bool validBase = false;
+            lang = "en";
+            year = 18;
+            serverAddress = IPAddress.Parse("127.0.0.1");
+            portNumber = 5678;
+            if (args.Length < 5)
             {
-                ConsoleWriter.Error("Args too short, must be provided: IP adress, socket number");
+                ConsoleWriter.Error("Args too short: YY-LANG-XX-cs --port [portnumber] --address [server IPv4 address or IPv6 address or host name]");
                 return false;
             }
-            var ip = IPAddress.Parse(args[0]);
-            if (ip == null)
+            int portId = 0, addressId = 0, baseId = 0;
+            try
             {
-                ConsoleWriter.Error("IP adress is not valid: " + args[0]);
-                valid = false;
+                for (int i = 0; i < args.Length; i++)
+                {
+                    switch (args[i])
+                    {
+                        case "--port":
+                            portId = ++i;
+                            portNumber = int.Parse(args[i]);
+                            if (portNumber >= 0)
+                                validPort = true;
+                            else
+                                portNumber = -1;
+                            break;
+                        case "--address":
+                            addressId = ++i;
+                            serverAddress = Dns.GetHostEntry(args[i]).AddressList[0];
+                            if (serverAddress != null)
+                                validAddress = true;
+                            break;
+                        default:
+                            baseId = i;
+                            if (Regex.IsMatch(args[i], "[0-9]{2}-LANG-(pl|en)-cs"))
+                            {
+                                var baseArgs = args[i].Split('-');
+                                year = int.Parse(baseArgs[0]);
+                                lang = baseArgs[2];
+                                validBase = true;
+                            }
+                            break;
+                    }
+                }
             }
-            var socket = Int32.Parse(args[1]);
-            if(socket <=0)
+            catch (Exception e)
             {
-                ConsoleWriter.Error("Socket number not valid: " + args[1]);
-                valid = false;
+                ConsoleWriter.Error("Error during reading args: \n" + e.Message);
+                return false;
             }
-            return valid;
+            if (!validAddress)
+                ConsoleWriter.Error("IP adress is not valid: " + args[addressId]);
+            if (!validPort)
+                ConsoleWriter.Error("Socket number not valid: " + args[portId]);
+            if (!validBase)
+                ConsoleWriter.Error("Invalid year and language param: " + args[baseId]);
+            return validAddress && validPort && validBase;
         }
     }
 }
