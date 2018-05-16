c85b94f8d93dc4735e096ab8cab39270fe49e24d
AleksWasiluk <alek.wasiluk@gmail.com>
2018-05-17 00:59:51 +0200

    Player script repaired

--- runPlayer.bat
+++ runPlayer.bat
@@ -1,2 +1,3 @@
 rem 127.0.0.1 5678 kolor %configurationPlayerFilePath%
-start dotnet \Agent\bin\Debug\netcoreapp2.0\Player.dll
+dotnet %cd%\Agent\bin\Debug\netcoreapp2.0\Player.dll 127.0.0.1 5678 blue %cd%\Championship.xml
+pause >nul
